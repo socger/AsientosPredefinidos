f310a0553b111c785a7e94c2e07d388236081571
Jerónimo Sánchez - socger <socger@gmail.com>
2021-08-27 13:05:53 +0200

    Ponemos a estos modelos y controladores contenido de licencia

--- Extension/Controller/ListAsiento.php
+++ Extension/Controller/ListAsiento.php
@@ -1,5 +1,21 @@
 <?php
-
+/**
+ * This file is part of AsientoPredefinido plugin for FacturaScripts
+ * Copyright (C) 2021 Carlos Garcia Gomez            <carlos@facturascripts.com>
+ *                    Jeronimo Pedro Sánchez Manzano <socger@gmail.com>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 namespace FacturaScripts\Plugins\AsientosPredefinidos\Extension\Controller;
 
 use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
