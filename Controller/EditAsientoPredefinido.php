c6d32b92dc9fc962b69f7c61ca341b36e48cd11a
Jerónimo Sánchez - socger <socger@gmail.com>
2021-08-27 11:43:08 +0200

    - Modificamos function createViewEditAsientoPredefinidoLinea(), le   quitamos los disableColumns que tenía (se hace desde su vista) - Modificamos case 'EditAsientoPredefinidoLinea' de function loadData() - Modificamos case default de function loadData() para que aparezca o no   el botón de generar asientos, según se esté creando la cabecera del   asiento predefinido o ya esté creada. - Creamos toda la parte de Action para generar el asiento (llama a   funciones del modelo AsientoPredefinido.php)

--- Controller/EditAsientoPredefinido.php
+++ Controller/EditAsientoPredefinido.php
@@ -1,10 +1,11 @@
 <?php
+
 namespace FacturaScripts\Plugins\AsientosPredefinidos\Controller;
 
 use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
 
-class EditAsientoPredefinido extends \FacturaScripts\Core\Lib\ExtendedController\EditController
-{
+class EditAsientoPredefinido extends \FacturaScripts\Core\Lib\ExtendedController\EditController {
+
     public function getModelClassName() {
         return "AsientoPredefinido";
     }
@@ -15,38 +16,66 @@ class EditAsientoPredefinido extends \FacturaScripts\Core\Lib\ExtendedController
         $pageData["icon"] = "fas fa-cogs";
         return $pageData;
     }
-    
+
     protected function createViews() {
         parent::createViews();
-        
+
         $this->createViewEditAsientoPredefinidoLinea();
-        
+
         $this->setTabsPosition('bottom'); // Las posiciones de las pestañas pueden ser left, top, down
     }
-    
-    protected function createViewEditAsientoPredefinidoLinea(string $viewName = 'EditAsientoPredefinidoLinea')
-    {
+
+    protected function createViewEditAsientoPredefinidoLinea(string $viewName = 'EditAsientoPredefinidoLinea') {
         $this->addEditListView($viewName, 'AsientoPredefinidoLinea', 'Asientos predefinidos - Líneas');
         $this->views[$viewName]->setInLine(true);
-
-        /// disable columns
-        $this->views[$viewName]->disableColumn('idasientopredefinido');
-        $this->views[$viewName]->disableColumn('idasientopredefinidolinea');
     }
-    
-    protected function loadData($viewName, $view)
-    {
+
+    protected function loadData($viewName, $view) {
         switch ($viewName) {
             case 'EditAsientoPredefinidoLinea':
-                $idasientopredefinido = $this->getViewModelValue($this->getMainViewName(), 'idasientopredefinido');
-                $where = [new DataBaseWhere('idasientopredefinido', $idasientopredefinido)];
-                $view->loadData('', $where, ['idasientopredefinido' => 'ASC', 'orden' => 'ASC']);
+                $idasientopre = $this->getViewModelValue($this->getMainViewName(), 'id');
+                $where = [new DataBaseWhere('idasientopre', $idasientopre)];
+                $view->loadData('', $where, ['orden' => 'ASC', 'idasientopre' => 'ASC']);
                 break;
 
             default:
                 parent::loadData($viewName, $view);
+
+                // Sólo si el registro existe se añade el botón
+                if ($view->model->exists()) {
+                    $this->addButton($viewName, [
+                        "action" => "gen-accounting",
+                        "color" => "success",
+                        "label" => "generate-accounting-entry",
+                        "icon" => "fas fa-magic",
+                        "type" => "modal"
+                    ]);
+                }
+
                 break;
         }
     }
-    
+
+    protected function execAfterAction($action) {
+        if ($action === 'gen-accounting') {
+            $this->generateAccountingAction();
+            return;
+        }
+
+        return parent::execAfterAction($action);
+    }
+
+    protected function generateAccountingAction() {
+        $date = (string) $this->request->request->get('date', '');
+        $idempresa = (int) $this->request->request->get('idempresa');
+
+        $asiento = $this->getModel()->generate($date, $idempresa);
+        if ($asiento->exists()) {
+            $this->toolBox()->i18nLog()->info("Generado correctamente el asiento");
+            $this->redirect($asiento->url(), 1); // El parámetro 1 es un temporizador en redireccionar, así el usuario ve el mensaje de la línea anterior
+        } else {
+            $this->toolBox()->i18nLog()->warning('No se pudo generar el asiento.');
+        }
+    }
+
 }
