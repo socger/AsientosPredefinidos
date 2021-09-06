ac21a3da290c401338e41e072f80af4bf43fa5f6
Jerónimo Sánchez - socger <socger@gmail.com>
2021-09-06 19:56:41 +0200

    Añadimos composer al init del plugin

--- Init.php
+++ Init.php
@@ -2,6 +2,8 @@
 
 namespace FacturaScripts\Plugins\AsientosPredefinidos;
 
+require_once __DIR__ . '/vendor/autoload.php';
+
 class Init extends \FacturaScripts\Core\Base\InitClass
 {
     public function init()
